c84b171ed078909e9bf17150938b812710fe23a0
guohao <guohao@mbcloud.com>
2019-07-13 18:36:54 +0800

    feat(handle response): handle response data
    add func with handling the response data in type of json

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -79,24 +79,49 @@ axios({
 // })
 
 
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   headers: {
+//     'content-type': 'application/json',
+//     'Accept': 'application/json,text/plain,*/*'
+//   },
+//   data: {
+//     a: 1,
+//     b: 2
+//   }
+// })
+
+// const paramsString = 'q=URLUtils.searchParams&topic=api'
+// const searchParams = new URLSearchParams(paramsString)
+
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   data: searchParams
+// })
+
+
+// tslint:disable-next-line: no-floating-promises
 axios({
   method: 'post',
   url: '/base/post',
-  headers: {
-    'content-type': 'application/json',
-    'Accept': 'application/json,text/plain,*/*'
-  },
   data: {
     a: 1,
     b: 2
   }
+}).then((res) => {
+  console.log(res)
 })
-
-const paramsString = 'q=URLUtils.searchParams&topic=api'
-const searchParams = new URLSearchParams(paramsString)
-
+// tslint:disable-next-line: no-floating-promises
 axios({
   method: 'post',
   url: '/base/post',
-  data: searchParams
+  responseType: 'json',
+  data: {
+    a: 3,
+    b: 4
+  }
+}).then((res) => {
+  console.log(res)
 })
